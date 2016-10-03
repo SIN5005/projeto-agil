fa047aeb633392a3138e3c05883bc9ba63a16204
HerikLyma <heriklyma@hotmail.com>
2016-10-03 20:46:24 -0300

    Eliminação das demais telas de usuário. Somente a tela de cadastro ficou.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  #resources :users
   resources :categories
   root 'welcome#index'
+  get 'users' => 'users#new'
+  post 'users' => 'users#create'
+  get "/users/show" => "users#show", as: :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
