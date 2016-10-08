5953fbdec18113700ad925db8d063355aeb125db
Bruno Casado <cryzon@hotmail.com>
2016-10-08 17:36:30 +0000

    Adicionado a funcionalidade CRUD para cadastro de contas

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006000827) do
+ActiveRecord::Schema.define(version: 20161008173019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.float    "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +59,7 @@ ActiveRecord::Schema.define(version: 20161006000827) do
     t.boolean  "locked",     default: false
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "transactions", "categories"
   add_foreign_key "transactions", "users"
 end
