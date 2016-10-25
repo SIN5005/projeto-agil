e406f7d1dac52bee9b3046880260b9a867c5ff2f
Ricardo Calegaro <ricardocalegaro@gmail.com>
2016-10-25 13:07:19 +0000

    Capybara step definitions

--- features/step_definitions/user_step.rb
+++ features/step_definitions/user_step.rb
@@ -1,39 +1,29 @@
-require 'watir-webdriver'
-
-browser = nil
-
 Given(/^I am on the Pluto homepage$/) do  
-	browser = Watir::Browser.new :firefox
-	browser.goto('http://localhost:3000')
+	visit(root_path)
 end
 
 When(/^click Cadastro$/) do  
-	browser.link(:text => "Cadastro").when_present.click
-	browser.windows.last.use
+	click_on('Cadastro')
 end
 
 Then(/^Users page shoud appears$/) do
-	puts 'Users page!'	
-	sleep(5)
-	browser.close
+	expect(page).to have_current_path(users_path+'/')
 end
 
 
 Given(/^I am on the Cadastro page$/) do
-	browser = Watir::Browser.new :firefox
-	browser.goto('http://localhost:3000/users')
+	visit(users_path)
+	sleep(2)
 end
 
 When(/^I fill name, email, password and password confirmation and click on create user$/) do
-	browser.text_field(:name => "user[name]").set 'Herik Lima'
-	browser.text_field(:name => "user[email]").set 'heriklyma@hotmail.com'
-	browser.text_field(:name => "user[password]").set '1234'
-	browser.text_field(:name => "user[password_confirmation]").set '1234'
-	browser.button(:name => 'commit').click
+	fill_in 'user[name]', :with => 'Herik Lima'
+	fill_in 'user[email]', :with => 'heriklyma@hotmail.com'
+	fill_in 'user[password]', :with => '1234'
+	fill_in 'user[password_confirmation]', :with => '1234'
+	click_on('Create User')
 end
 
 Then(/^save with shoud appears$/) do
-	puts 'Salvo com sucesso.'   	
-	sleep(5)
-	browser.close
+	expect(page).to have_content('Salvo com sucesso.')
 end
--- features/step_definitions/welcome_step.rb
+++ features/step_definitions/welcome_step.rb
@@ -1,18 +1,11 @@
-require 'watir-webdriver'
-
-browser = nil
-
 Given(/^I am on the Browser$/) do
-	browser = Watir::Browser.new :firefox
 	puts 'Browser'
 end
 
 When(/^type Pluto site link$/) do  
-	browser.goto('http://localhost:3000')
+	visit(root_path)
 end
 
 Then(/^Pluto homepage shoud appears$/) do
-	puts 'Pluto - Financial App!'
-	sleep(5)
-	browser.close
+	expect(page).to have_current_path(root_path)
 end
