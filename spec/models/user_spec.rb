617304eb5c8ba4037aaf429535b90236f6363e58
HerikLyma <heriklyma@hotmail.com>
2016-10-26 11:08:38 -0200

    Melhoria no código user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-     context "Salvar usuário" do
-          it "Salvo com sucesso" do
-               user          = User.new
-               user.name     = "Herik"
-               user.email    = "Herik@hotmail.com"
-               user.password = "1234"
+     before(:each) do
+          @user          = User.new
+          @user.name     = "Herik"
+          @user.email    = "Herik@hotmail.com"
+          @user.password = "1234"          
+     end
 
-               result = user.save
+     context "Salvar usuário" do
+          it "Salvo com sucesso" do               
+               result = @user.save
 
                expect(result[0]).to be_truthy
                expect(result[1]).to include("Salvo com sucesso.")
           end
           it "Erro ao salvar." do
-               user          = User.new
-               
-               result = user.save
+               @user.name = nil
+               result = @user.save
 
                expect(result[0]).to be_falsey
                expect(result[1]).to include("Erro ao salvar.")
@@ -25,43 +26,31 @@ RSpec.describe User, type: :model do
      
      context "E-mail" do
           it "E-mail existe" do
-               user          = User.new
-               user.name     = "Herik"
-               user.email    = "Herik@hotmail.com"
-               user.password = "1234"
-
-               result = user.save
+               result = @user.save
 
                expect(result[0]).to be_truthy
                expect(result[1]).to include("Salvo com sucesso.")
-               expect(user.email_already_registered).to be_truthy
+               expect(@user.email_already_registered).to be_truthy
           end
-          it "E-mail não existe" do
-               user          = User.new               
-               user.email    = "Herik@hotmail.com.br"
-               expect(user.email_already_registered).to be_falsey
+          it "E-mail não existe" do               
+               @user.email    = "Herik@hotmail.com.br"
+               expect(@user.email_already_registered).to be_falsey
           end
      end
 
      context "Login" do
           it "Login com sucesso" do
-               user          = User.new
-               user.name     = "Herik"
-               user.email    = "Herik@hotmail.com"
-               user.password = "1234"
-
-               result = user.save
+               result = @user.save
 
                expect(result[0]).to be_truthy
                expect(result[1]).to include("Salvo com sucesso.")
 
-               expect(user.login).to be_truthy
+               expect(@user.login).to be_truthy
           end
-          it "Login falha" do         
-               user          = User.new 
-               user.email    = "Herik@lima.com"
-               user.password = "123456"
-               expect(user.login).to be_falsey
+          it "Login falha" do                        
+               @user.email    = "Herik@lima.com"
+               @user.password = "123456"
+               expect(@user.login).to be_falsey
           end
      end
 end
