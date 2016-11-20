72f9934fdc3fa294911be70d68e45e90184daa22
Lucas Lima <lcs.freirelima@gmail.com>
2016-11-20 21:46:25 -0200

    correcao dos testes para a model Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ApplicationRecord
+  has_one :user, class_name: "User"
+  has_one :account, class_name: "Account"
+  
   validates_presence_of :user_id, :account_id, :amount
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  context "without DB" do
-    it "invalid without data" do
-      transac = Transaction.new()
-      expect(transac).to_not be_valid
-    end
-    it "invalid without account and user" do
-      transac = Transaction.new(:amount => 1.54)
-      expect(transac).to_not be_valid
-    end
-    it "invalid without account" do
-      transac = Transaction.new(user_id: 1, :amount => 1.54)
-      expect(transac).to_not be_valid
-    end
-    it "invalid without user" do
-      transac = Transaction.new(account_id: 1, :amount => 1.54)
-      expect(transac).to_not be_valid
-    end
-    it "valid with all data" do
-      transac = Transaction.new(user_id: 1, account_id: 1, :amount => 1.54)
-      expect(transac).to be_truthy
-    end
+  it "invalid without data" do
+    transac = Transaction.create()
+    expect(transac).to_not be_valid
+  end
+  it "invalid without account and user" do
+    transac = Transaction.create(:amount => 1.54)
+    expect(transac).to_not be_valid
+  end
+  it "invalid without account" do
+    transac = Transaction.create(user_id: 1, :amount => 1.54)
+    expect(transac).to_not be_valid
+  end
+  it "invalid without user" do
+    transac = Transaction.create(account_id: 1, :amount => 1.54)
+    expect(transac).to_not be_valid
+  end
+  it "valid with all data" do
+    transac = Transaction.create(user_id: 1, account_id: 1, :amount => 1.54)
+    expect(transac).to be_valid
+  end
+
+  it "has one user" do
+    assoc = described_class.reflect_on_association(:user)
+    expect(assoc.macro).to eq :has_one
+  end
+
+  it "has one account" do
+    assoc = described_class.reflect_on_association(:account)
+    expect(assoc.macro).to eq :has_one
   end
 end
