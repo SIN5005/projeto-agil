0915c11422fd6b46ffbd577ab1c242f5ae95541c
Bruno Casado <cryzon@hotmail.com>
2016-11-20 14:32:36 +0000

    Arquivos de BDD da funcionalidade de Contas

---
+++ features/account.feature
@@ -0,0 +1,32 @@
+Feature: Account page
+	In order to manage the accounts
+	As an explicit system actor
+	I want to see the account page
+
+	Scenario: See Account page
+
+	Given I am loged in Pluto Homepage
+
+	When click account link
+
+	Then account page shoud appears
+	
+	
+	
+	Scenario: Create an account
+
+	Given I am on the Account page
+
+	When I fill name and balance and click on create account
+
+	Then save with shoud appears
+	
+	
+	
+	Scenario: Delete an account
+
+	Given I am on the Account page
+
+	When I fill name click on delete account
+
+	Then save with shoud appears
---
+++ features/step_definitions/account_step.rb
@@ -0,0 +1,36 @@
+require 'watir-webdriver'
+
+browser = nil
+
+Given(/^I am loged in Pluto homepage$/) do  	
+	browser = Watir::Browser.new :firefox
+	browser.goto('http://localhost:3000/home')
+end
+
+When(/^click Account$/) do  
+	browser.link(:text => "Contas").when_present.click	
+end
+
+Then(/^Accounts page shoud appears$/) do
+	puts 'Accounts page!'	
+	sleep(5)
+	browser.close
+end
+
+
+Given(/^I am on the Account page$/) do
+	browser = Watir::Browser.new :firefox
+	browser.goto('http://localhost:3000/account')
+end
+
+When(/^I fill name and balance and click on create account$/) do
+	browser.text_field(:name => "account[name]").set 'Cartão de Crédito'
+	browser.text_field(:name => "account[balance]").set '0.00'
+	browser.button(:name => 'commit').click
+end
+
+Then(/^save with shoud appears$/) do
+	puts 'Salvo com sucesso.'   	
+	sleep(5)
+	browser.close
+end
