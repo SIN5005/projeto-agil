5526bf94aedbf26ed9c8bb51c58a3d5c6ab78d85
Bruno Casado <cryzon@hotmail.com>
2016-11-29 00:53:15 +0000

    ajuste no modelo de contas

--- db/migrate/20161120203512_create_accounts.rb
+++ db/migrate/20161120203512_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
       t.string :name
-
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120204226) do
+ActiveRecord::Schema.define(version: 20161129004544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161120204226) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "balance"
   end
 
   create_table "transactions", force: :cascade do |t|
