17648ee5be4a7f2a51cfdd905ee20b786a289c37
Bruno Casado <cryzon@hotmail.com>
2016-11-30 00:33:30 +0000

    Criação da Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do  
 	root 'welcome#index'
+	get 'home' => 'welcome#home'
 	get 'users' => 'users#index'
 	post 'users' => 'users#create'
 	get 'login' => 'login#index'
 	post 'login' => 'login#login'
 	get 'transactions/new' => 'transactions#new'
-	get "accounts" => "accounts#index"
+	get 'accounts' => 'accounts#index'
 end
 
