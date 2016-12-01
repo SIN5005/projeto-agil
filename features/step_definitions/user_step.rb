b838156302efe51d84500b9c07e37c59d488d26a
HerikLyma <heriklyma@hotmail.com>
2016-12-01 19:08:51 -0200

    BDD para Alterar senha]

--- features/step_definitions/user_step.rb
+++ features/step_definitions/user_step.rb
@@ -1,6 +1,7 @@
 require 'watir-webdriver'
 
 browser = nil
+generatedEmail = nil
 
 Given(/^I am on the Pluto homepage$/) do  	
 	browser = Watir::Browser.new :firefox
@@ -24,9 +25,10 @@ Given(/^I am on the Cadastro page$/) do
 end
 
 When(/^I fill name, email, password and password confirmation and click on create user$/) do
-	number = rand(1...100)
+	number = rand(1...10000)
+	generatedEmail = "heriklyma#{number}@hotmail.com"
 	browser.text_field(:name => "user[name]").set 'Herik Lima'
-	browser.text_field(:name => "user[email]").set "heriklyma#{number}@hotmail.com"
+	browser.text_field(:name => "user[email]").set generatedEmail
 	browser.text_field(:name => "user[password]").set '1234'
 	browser.text_field(:name => "user[password_confirmation]").set '1234'
 	browser.button(:name => 'commit').click
@@ -37,3 +39,23 @@ Then(/^save with success shoud appears$/) do
 	sleep(5)
 	browser.close
 end
+
+
+Given(/^I log into the system$/) do
+	browser = Watir::Browser.new :firefox
+	browser.goto('http://localhost:3000/login')
+	browser.text_field(:name => "email").set generatedEmail
+	browser.text_field(:name => "password").set '1234'
+	browser.button(:name => 'entrar').click	
+	sleep(2)
+end
+
+When(/^I click on Change Password$/) do	
+	browser.link(:text => "Alterar Senha").when_present.click	
+end
+
+Then(/^change password should appears$/) do	
+	expect(browser.text.include?("Alterar Senha")).to be_truthy	
+	sleep(5)
+	browser.close
+end	
