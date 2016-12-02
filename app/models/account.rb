0fc589d7e366d5bcf753f78bb6dbce22aafa6a82
Bruno Casado <cryzon@hotmail.com>
2016-12-02 01:13:23 +0000

    criacao inicial dos testes

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,9 @@ class Account < ApplicationRecord
     has_one :user, class_name: "User"
     
     validates :name, presence: true
+    
+    def titulo_completo
+        "Titulo completo com o nome do cartao"
+    end
+    
 end
