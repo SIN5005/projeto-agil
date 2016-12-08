d95f958914384925008f86e6ae9bab50c88b3a58
Bruno Casado <cryzon@hotmail.com>
2016-12-08 23:44:58 +0000

    alteracao no accounts_controller_specs para usar before

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,19 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AccountsController, type: :controller do
-#    before(:each) do
-#        @user          = User.new
-#        @user.name     = "Bruno Casado"
-#        @user.email    = "brncasado@gmail.com"
-#        @user.password = "1234"
-#        @user.save
-#        @user.login
-#        
-#        puts "Teste"
-#    end
+    before(:each) do
+        @request.session[:user_id] = 1
+    end
     describe "GET index" do
         it "renders the index template" do
-            @request.session[:user_id] = 1
             get :index
             expect(response).to render_template("index")
             expect(response.body).to include("Saldo")
@@ -23,13 +15,11 @@ RSpec.describe AccountsController, type: :controller do
 
     describe "POST #create" do 
         it "shoud return O campo 'Nome' deve ser preenchido" do
-            @request.session[:user_id] = 1
             post :create, account: {name: '', balance: 10.0, user_id: 1}
             expect( subject.request.flash[:notice] ).to include("O campo 'Nome' deve ser preenchido")
         end
 
         it "shoud return Salvo com sucesso e Nome da conta já utilizado" do
-            @request.session[:user_id] = 1
             post :create, account: {name: 'Visa', balance: 10.0, user_id: 1}
             expect( subject.request.flash[:notice] ).to include("Salvo com sucesso")
 
@@ -38,7 +28,6 @@ RSpec.describe AccountsController, type: :controller do
         end
 
         it "shoud return O campo 'Saldo' deve ser preenchido" do
-            @request.session[:user_id] = 1
             post :create, account: {name: 'Visa', balance: '', user_id: 1}
             expect( subject.request.flash[:notice] ).to include("O campo 'Saldo' deve ser preenchido")
         end
